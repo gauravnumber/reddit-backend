faf1139f1e8fc0ea3d69ba93a64cd9fb071750a9
Gaurav Singh <gauravnumber@gmail.com>
2021-12-15 12:06:00 +0530

    user modulize

--- schema/resolvers/mutation.js
+++ schema/resolvers/mutation.js
@@ -2,54 +2,13 @@ const { AuthenticationError, UserInputError } = require('apollo-server')
 
 const checkAuth = require('@context/check-auth')
 
-const User = require('@models/userSchema')
 const Post = require('@models/postSchema')
 
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const user = require('./user')
 
 module.exports = {
   Mutation: {
-    register: async (_, { username, password }) => {
-      const passwordHash = await bcrypt.hash(password, 12)
-
-      const newUser = new User({
-        username,
-        passwordHash,
-      })
-
-      const res = await newUser.save()
-
-      const token = jwt.sign({
-        _id: res._id,
-        username: res.username,
-      }, 'SECRET')
-
-      return { token, ...res._doc }
-      // return { id: res._id, token, ...res._doc }
-    },
-
-    login: async (_, { username, password }) => {
-      const user = await User.findOne({ username })
-
-      if (!user) {
-        throw new UserInputError("Username is not registered.")
-      }
-
-      const isPassword = await bcrypt.compare(password, user.passwordHash)
-
-      if (!isPassword) {
-        throw new AuthenticationError('Password is incorrect.')
-      }
-
-      const token = jwt.sign({
-        _id: user._id,
-        username: user.username,
-      }, 'SECRET')
-
-      return { token, ...user._doc }
-      // return { id: user._id, token, ...user._doc }
-    },
+    ...user.Mutation,
 
     post: async (_, { title, body }, context) => {
       const loginUser = checkAuth(context)
---
+++ schema/resolvers/user.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const User = require('@models/userSchema')
+
+module.exports = {
+  Mutation: {
+    register: async (_, { username, password }) => {
+      const passwordHash = await bcrypt.hash(password, 12)
+
+      const newUser = new User({
+        username,
+        passwordHash,
+      })
+
+      const res = await newUser.save()
+
+      const token = jwt.sign({
+        _id: res._id,
+        username: res.username,
+      }, 'SECRET')
+
+      return { token, ...res._doc }
+      // return { id: res._id, token, ...res._doc }
+    },
+
+    login: async (_, { username, password }) => {
+      const user = await User.findOne({ username })
+
+      if (!user) {
+        throw new UserInputError("Username is not registered.")
+      }
+
+      const isPassword = await bcrypt.compare(password, user.passwordHash)
+
+      if (!isPassword) {
+        throw new AuthenticationError('Password is incorrect.')
+      }
+
+      const token = jwt.sign({
+        _id: user._id,
+        username: user.username,
+      }, 'SECRET')
+
+      return { token, ...user._doc }
+      // return { id: user._id, token, ...user._doc }
+    },
+
+  }
+}
