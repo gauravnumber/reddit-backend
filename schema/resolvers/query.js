674b53be96048df5d749399dbd76c021bf135203
Gaurav Singh <gauravnumber@gmail.com>
2021-12-19 22:35:26 +0530

    top of upvotes supported

--- schema/resolvers/query.js
+++ schema/resolvers/query.js
@@ -8,20 +8,37 @@ module.exports = {
   Query: {
     show: () => "laugh",
     getSubredditPost: async (_, { subredditId, sort = 'hot' }) => {
-
       const subreddit = await Subreddit.findById(subredditId).populate('post')
-      // return subreddit.post 
-      // return subreddit.post.sort(sortBy('createdAt'))
+
       if (sort === 'hot') {
         return subreddit.post.sort(sortByDesc('createdAt'))
-      } else if (sort === 'top') {
-        // console.log('subreddit.post', subreddit.post)
+      } else if (sort === 'top:alltime') {
         let filterPost = subreddit.post.filter(p => p.upvote.length > 0)
 
         let filterPostSort = filterPost.sort((a, b) => (a['upvote'].length > b['upvote'].length) ? -1 : 1)
 
         return filterPostSort
-        // return subreddit.post
+      } else if (sort === 'top:week') {
+        let filterPost = subreddit.post
+
+        filterPost = filterPost.filter(post => post.createdAt > Date.now() - 1000 * 3600 * 24 * 7)
+          .filter(p => p.upvote.length > 0).sort((a, b) => b.upvote.length - a.upvote.length)
+
+        return filterPost
+      } else if (sort === 'top:day') {
+        let filterPost = subreddit.post
+
+        filterPost = filterPost.filter(post => post.createdAt > Date.now() - 1000 * 3600 * 24)
+          .filter(p => p.upvote.length > 0).sort((a, b) => b.upvote.length - a.upvote.length)
+
+        return filterPost
+      } else if (sort === 'top:month') {
+        let filterPost = subreddit.post
+
+        filterPost = filterPost.filter(post => post.createdAt > Date.now() - 1000 * 3600 * 24 * 7 * 30)
+          .filter(p => p.upvote.length > 0).sort((a, b) => b.upvote.length - a.upvote.length)
+
+        return filterPost
       }
     }
 
