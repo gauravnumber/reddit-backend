cba380092c5c7edcd5a705f3fe6c27d9cbf01cb9
Gaurav Singh <gauravnumber@gmail.com>
2021-12-21 21:03:57 +0530

    upvote user solved problem, num. of upvote solved

--- schema/resolvers/query.js
+++ schema/resolvers/query.js
@@ -1,7 +1,7 @@
 // const User = require('@models/userSchema')
 const Subreddit = require('@models/subredditSchema')
 const { sortByDesc } = require('@utils')
-// const User = require('@/models/userSchema')
+const User = require('@models/userSchema')
 // const User = require('../../models/userSchema')
 
 module.exports = {
@@ -43,13 +43,49 @@ module.exports = {
     }
 
   },
-  // Post: {
-  //   owner: async (parent) => {
-  //     const loginUser = await User.findById(parent._id)
+  Post: {
+    owner: async (parent) => {
+      // return parent + 'hello owner'
+      // console.log(parent._id)
+      // console.log('parent', parent)
+      const loginUser = await User.findById(parent.owner)
 
-  //     console.log('loginUser._doc', loginUser._doc)
-  //     return loginUser
-  //   }
-  // },
+      // console.log(loginUser)
+
+      // console.log('loginUser._doc', loginUser._doc)
+      return loginUser
+    },
+    upvote: async (parent) => {
+      const upvoteUser = []
+
+      for (let index = 0; index < parent.upvote.length; index++) {
+        upvoteUser.push(await User.findById(parent.upvote[index]))
+      }
+
+      return upvoteUser
+    },
+    downvote: async (parent) => {
+      const downvoteUser = []
+
+      for (let index = 0; index < parent.downvote.length; index++) {
+        downvoteUser.push(await User.findById(parent.downvote[index]))
+      }
+
+      return downvoteUser
+    },
+    vote: async (parent) => {
+      const voteUser = []
+
+      for (let index = 0; index < parent.vote.length; index++) {
+        voteUser.push(await User.findById(parent.vote[index]))
+      }
+
+      return voteUser
+    },
+
+    totalNumOfVote: (parent) => {
+      return parent.upvote.length - parent.downvote.length
+    }
+  },
 
 }
