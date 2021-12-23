36c9d091595b0b7da3df77e132fb5d0952e06483
Gaurav Singh <gauravnumber@gmail.com>
2021-12-23 21:11:27 +0530

    done

--- schema/resolvers/query/query.js
+++ schema/resolvers/query/query.js
@@ -1,11 +1,13 @@
 // const User = require('@models/userSchema')
 const getSubredditPost = require('./getSubredditPost')
+const getPostsByUser = require('./getPostsByUser')
 const post = require('./post')
 
 module.exports = {
   Query: {
     show: () => "laugh",
     ...getSubredditPost.Query,
+    ...getPostsByUser.Query,
   },
   Post: {
     ...post.Post
