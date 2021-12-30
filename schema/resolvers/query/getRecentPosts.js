6195f0d575c46ae6d913dd61cc9a51e322021076
Gaurav Singh <gauravnumber@gmail.com>
2021-12-30 15:28:05 +0530

    done

--- schema/resolvers/query/getRecentPosts.js
+++ schema/resolvers/query/getRecentPosts.js
@@ -3,8 +3,8 @@ const Post = require('@models/postSchema')
 module.exports = {
   Query: {
     getRecentPosts: async () => {
-      const post = await Post.find().populate('owner')
-      // console.log('post', post)
+      const post = await Post.find()
+
       return post
     }
   }
