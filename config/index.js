9555c14d87b7b8871ca9a16a7c0bc85cb9131749
Gaurav Singh <gauravnumber@gmail.com>
2022-01-03 22:12:13 +0530

    MONGODB_URI added in .env

--- config/index.js
+++ config/index.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
 
 const SECRET = process.env.SECRET
+const MONGODB_URI = process.env.MONGODB_URI
 
-module.exports = { SECRET }
+module.exports = {
+  SECRET,
+  MONGODB_URI,
+}
