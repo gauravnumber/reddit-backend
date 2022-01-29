51fecda91ca2859b8480547fc6a59c4b9cae0c63
Gaurav Singh <gauravnumber@gmail.com>
2022-01-29 13:52:25 +0530

    module of typeDefs done

--- schema/typeDefs/index.js
+++ schema/typeDefs/index.js
@@ -1,9 +1,11 @@
 const { gql } = require('apollo-server')
 
+const type = require('./type')
 const query = require('./query')
 const mutation = require('./mutation')
 
 module.exports.typeDefs = gql`
+  ${type}
   ${query}
   ${mutation}
 `
