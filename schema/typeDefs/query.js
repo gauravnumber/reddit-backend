2619407eb73ff5ad19ac22c8d3c6580b0933cba7
Gaurav Singh <gauravnumber@gmail.com>
2022-01-29 13:52:25 +0530

    module of typeDefs done

--- schema/typeDefs/query.js
+++ schema/typeDefs/query.js
@@ -1,41 +1,4 @@
 module.exports = `#graphql
-  type User {
-    _id: ID!
-    username: String!
-    token: String!
-    post: [Post]!
-  }
-
-  type Comment {
-    _id: ID!
-    body: String!
-    owner: User!
-    upvote: [User]!
-    downvote: [User]!
-    totalNumOfVotes: Int!
-    comment: [Comment]!
-  }
-
-  type Post {
-    _id: ID!
-    title: String!
-    body: String!
-    owner: User!
-    vote: [User]!
-    upvote: [User]!
-    downvote: [User]!
-    totalNumOfVotes: Int!
-    comment: [Comment]!
-    createdAt: String!
-    subreddit: Subreddit!
-  }
-  
-  type Subreddit {
-    _id: ID!
-    name: String!
-    post: [Post]!
-  }
-
   type Query {
     show: String!
     getSubredditPost(
