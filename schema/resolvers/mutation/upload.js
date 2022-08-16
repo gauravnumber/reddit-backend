053eb07c4ba614d9d4fdd15eec2ac6bcbd98f1ac
Gaurav Singh <gauravnumber@gmail.com>
2022-08-16 11:12:53 +0530

    crypto add for unique image name

--- schema/resolvers/mutation/upload.js
+++ schema/resolvers/mutation/upload.js
@@ -7,9 +7,11 @@ module.exports = {
   Mutation: {
     singleUpload: async (parent, { file }) => {
       // console.log('parent', parent)
-      console.log('file', file)
+      // console.log('file', file)
+      // console.log('crypto', crypto.randomBytes(20).toString('hex'))
       const { createReadStream, filename, mimetype, encoding } = await file;
 
+      console.log(filename, mimetype, encoding)
       // Invoking the `createReadStream` will return a Readable Stream.
       // See https://nodejs.org/api/stream.html#stream_readable_streams
       const stream = createReadStream();
@@ -19,7 +21,8 @@ module.exports = {
       // const out = require('fs').createWriteStream('local-file-output.txt');
       // const out = require('fs').createWriteStream('bar.jpg');
       // const out = require('fs').createWriteStream(`${crypto.randomUUID({ disableEntropyCache: true })}.jpg`);
-      const out = require('fs').createWriteStream(`${(Math.random() + 1).toString(36).substring(2)}.jpg`);
+      // const out = require('fs').createWriteStream(`${(Math.random() + 1).toString(36).substring(2)}.jpg`);
+      const out = require('fs').createWriteStream(`${crypto.randomBytes(20).toString('hex')}.jpg`);
 
       stream.pipe(out);
       // await finished(out);
